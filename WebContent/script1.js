5af04722bdcd957d5a9133036aefb0a8a06e241f
Javier Delgado <javier_180796@hotmail.com>
2019-04-23 09:56:59 -0400

    Started 3rd Project

--- WebContent/script1.js
+++ WebContent/script1.js
@@ -7,37 +7,45 @@ var DataTrick = null;
 
 function upData(){
 	NormalData = $('dataSend').value;
-  DataTrick = "%" + $('dataSend').value + "%";
-  var data = {
-	dataTrick: DataTrick,
-  };
-  let config = {
-    method: 'POST',
-    body: JSON.stringify(data),
-  };
-  fetch("./Filter", config)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      alert(data.message);
-      var item2;
-      if(data.status == 200 && data.status != undefined){
-        $("disabled").value = data.message;
-        for (var i = 0; i < data.arraysFound[0].length; i++){
-        	item2 = document.createElement("h6");
-        	item2.innerHTML = "Found: " + data.arraysFound[0][i] + " It was found: " + data.arraysFound[1][i] + " before.";
-        	$("wordsFound").appendChild(item2);
-        	$("wordsFound").style.display = "block";
-        }
-        alert("The words down are the result(s). If you want an update about most wanted, reload the page.")
-      } else {
-    	alert("Oops..! Something is wrong. Reload the page. Also you can try another word");
-      }
-    })
-    .catch((e) => {
-      console.log(e)
-    })
+	DataTrick = "%" + $('dataSend').value + "%";
+	var data = {
+		dataTrick: DataTrick,
+	};
+	let config = {
+		method: 'POST',
+		body: JSON.stringify(data),
+	};
+	fetch("./Filter", config)
+    	.then(function(response){
+    		return response.json();
+    	})
+    	.then(function(data){
+    		alert(data.message);
+    		var item2;
+    		if(data.status == 200 && data.status != undefined){
+    			$("disabled").value = data.message;
+    			for (var i = 0; i < data.arraysFound[0].length; i++){
+    				console.log(data.arraysFound[0][i].includes(NormalData));
+    				if((data.arraysFound[0][i].includes(" " + NormalData + " ")) || (data.arraysFound[0][i].includes(" " + NormalData))){
+			        	item2 = document.createElement("h6");
+			        	item2.innerHTML = "Found: " + data.arraysFound[0][i] + " It was found: " + data.arraysFound[1][i] + " before.";
+			        	$("wordsFound").appendChild(item2);
+			        	$("wordsFound").style.display = "block";
+    				} /*else if(!data.arraysFound[0][i].includes(" " + NormalData)){
+    					item2 = document.createElement("h6");
+			        	item2.innerHTML = "Found: " + data.arraysFound[0][i] + " It was found: " + data.arraysFound[1][i] + " before.";
+			        	$("wordsFound").appendChild(item2);
+			        	$("wordsFound").style.display = "block";
+    				}*/
+		        }
+    			alert("The words down are the result(s). If you want an update about most wanted, reload the page.")
+    		} else {
+    			alert("Oops..! Something is wrong. Reload the page. Also you can try another word");
+    		}
+    	})
+	    .catch((e) => {
+	    	console.log(e)
+	    })
 }
 
 function forcedLowerCase(){
